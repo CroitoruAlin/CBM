cb8c4349f356679a1e54e7df56e045e9536cb333
Jarca Andrei <69394169+JarcaAndrei@users.noreply.github.com>
2024-07-06 15:11:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -12,13 +12,13 @@ def test(model, device, test_loader):
             data, target = data.to(device), target.to(device)
             output = model(data, 0.)
             loss = F.cross_entropy(output, target)
-            '''
+            
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
             '''
             top5_pred = output.topk(5, dim=1).indices
             correct += sum([1 if target[i] in top5_pred[i] else 0 for i in range(len(target))])
-            
+            '''
             final_loss += loss.item()
             batch_num += 1
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(final_loss / batch_num, correct,
